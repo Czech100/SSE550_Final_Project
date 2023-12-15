ebcd2b14c999da3baeb8adef22b53c6288737136
Czech100 <50641600+Czech100@users.noreply.github.com>
2023-12-15 01:03:33 -0500

    Delete SSE550_Kaleb_Kushinka_Final_Project/SSE550_Kaleb_Kushinka_Final_Project directory

--- SSE550_Kaleb_Kushinka_Final_Project/SSE550_Kaleb_Kushinka_Final_Project/main.py
+++
@@ -1,9 +0,0 @@
-
-from gui import run_app
-from appointment import Appointment
-
-def main():
-    run_app()
-
-if __name__ == "__main__":
-    main()
--- SSE550_Kaleb_Kushinka_Final_Project/SSE550_Kaleb_Kushinka_Final_Project/node.py
+++
@@ -1,4 +0,0 @@
-class Node:
-    def __init__(self, appointment):
-        self.appointment = appointment
-        self.next = None
